74cd2bc430e7cf09ab67d347bc4713be4cc878e2
hanabuu <hanabusa.tatsuo0119@gmail.com>
2024-04-17 14:57:19 +0900

    add vuetify

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,16 +3,8 @@ import Home from "../components/Home.vue";
 import About from "../components/About.vue";
 
 const routes = [
-    {
-        path: "/home",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About,
-    },
+    {path: "/",name: "Home",component: Home},
+    {path: "/about",name: "About",component: About},
 ];
 
 const router = createRouter({
---
+++ src/vuetify.ts
@@ -0,0 +1,11 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as labsComponents from 'vuetify/labs/components'
+
+export default createVuetify({
+  components: {
+    ...components,
+    ...labsComponents,
+  },
+})
